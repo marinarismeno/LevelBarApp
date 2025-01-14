9db762e9d0e51641298b9597fbb0a0f05b6dbec7
MARINA\marinarismeno <marinayannaraki@gmail.com>
2025-01-14 06:30:22 +0200

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="MainWindow.xaml.cs" company="VIBES.technology">
+// Copyright (c) VIBES.technology. All rights reserved.
+// </copyright>
+
+namespace LevelBarApp
+{
+    using System.Windows;
+    using LevelBarApp.ViewModels;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            DataContext = new MainWindowViewModel();
+            InitializeComponent();
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,85 @@
+﻿// <copyright file="MainWindowViewModel.cs" company="VIBES.technology">
+// Copyright (c) VIBES.technology. All rights reserved.
+// </copyright>
+
+namespace LevelBarApp.ViewModels
+{
+    using System.Collections.ObjectModel;
+    using GalaSoft.MvvmLight;
+    using GalaSoft.MvvmLight.Command;
+    using LevelBarGeneration;
+
+    /// <summary>
+    /// MainWindowViewModel
+    /// </summary>
+    /// <seealso cref="ViewModelBase" />
+    public class MainWindowViewModel : ViewModelBase
+    {
+        // Fields
+        private readonly LevelBarGenerator levelBarGenerator;
+        private RelayCommand connectToGeneratorCommand;
+        private RelayCommand disconnectToGeneratorCommand;
+
+        // Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            levelBarGenerator = LevelBarGenerator.Instance;
+
+            levelBarGenerator.GeneratorStateChanged += LevelBarGenerator_GeneratorStateChanged;
+            levelBarGenerator.ChannelAdded += LevelBarGenerator_ChannelAdded;
+            levelBarGenerator.ChannelLevelDataReceived += LevelBarGenerator_ChannelDataReceived;
+            levelBarGenerator.ChannelRemoved += LevelBarGenerator_ChannelRemoved;
+        }
+
+        // Properties
+
+        /// <summary>
+        /// Gets or sets the level bars, one for each channel.
+        /// </summary>
+        /// <value>
+        /// The level bars.
+        /// </value>
+        public ObservableCollection<LevelBarViewModel> LevelBars { get; set; } = new ObservableCollection<LevelBarViewModel>();
+
+        /// <summary>
+        /// Gets the command to connect the generator
+        /// </summary>
+        /// <value>
+        /// The connect generator.
+        /// </value>
+        public RelayCommand ConnectGeneratorCommand => connectToGeneratorCommand ?? (connectToGeneratorCommand = new RelayCommand(new System.Action(async () => await levelBarGenerator.Connect())));
+
+        /// <summary>
+        /// Gets the command to disconnect the generator
+        /// </summary>
+        /// <value>
+        /// The disconnect generator.
+        /// </value>
+        public RelayCommand DisconnectGeneratorCommand => disconnectToGeneratorCommand ?? (disconnectToGeneratorCommand = new RelayCommand(new System.Action(async () => await levelBarGenerator.Disconnect())));
+
+        // Methods
+        private void LevelBarGenerator_ChannelAdded(object sender, ChannelChangedEventArgs e)
+        {
+            // Generate a LevelBarViewModel
+        }
+
+        private void LevelBarGenerator_ChannelRemoved(object sender, ChannelChangedEventArgs e)
+        {
+            // Remove the corresponding LevelBarViewModel
+        }
+
+        private void LevelBarGenerator_GeneratorStateChanged(object sender, GeneratorStateChangedEventArgs e)
+        {
+            // TODO Set the state of the generator
+        }
+
+        private void LevelBarGenerator_ChannelDataReceived(object sender, ChannelDataEventArgs e)
+        {
+            // TODO this is where the level data is coming in
+        }
+    }
+}
